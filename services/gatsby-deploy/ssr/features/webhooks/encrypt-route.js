4ff3f8ba32058d4a43812e8821eddebd6498877d
marcopeg <marco.pegoraro@gmail.com>
2019-03-17 10:24:50 +0100

    fix encrypt

--- services/gatsby-deploy/ssr/features/webhooks/encrypt-route.js
+++ services/gatsby-deploy/ssr/features/webhooks/encrypt-route.js
@@ -5,11 +5,11 @@ export const makeEncryptRoute = (settings) => {
     return async (req, res) => {
 
         const cipher = crypto.createCipher(settings.crypto.algorithm, settings.jwt.secret)
-        let crypted = cipher.update(req.data.github.auth.password, 'utf8', 'hex')
+        let crypted = cipher.update(req.data.token.auth.password, 'utf8', 'hex')
         crypted += cipher.final('hex')
 
-        req.data.github.auth.password = crypted
+        req.data.token.auth.password = crypted
 
-        res.send(await jwt.sign(req.data.github))
+        res.send(await jwt.sign(req.data.token))
     }
 }
